05180b263c89b6b3e4d6ac77c1b682f6ae22ff14
aanuk <mahataanuk@gmail.com>
2023-06-21 21:25:21 +0545

    second commit

--- script.js
+++ script.js
@@ -2,26 +2,62 @@ const searchBox = document.querySelector(".search-box");
 const searchBtn = document.querySelector(".search-icon");
 const cancelBtn = document.querySelector(".cancel-icon");
 const searchInput = document.querySelector("input");
-const searchData = document.querySelector(".search-data");
-searchBtn.onclick =()=>{
+searchBtn.onclick = () => {
   searchBox.classList.add("active");
   searchBtn.classList.add("active");
   searchInput.classList.add("active");
   cancelBtn.classList.add("active");
   searchInput.focus();
-  if(searchInput.value != ""){
-    var values = searchInput.value;
-    searchData.classList.remove("active");
-    searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
-  }else{
-    searchData.textContent = "";
-  }
-}
-cancelBtn.onclick =()=>{
+};
+cancelBtn.onclick = () => {
   searchBox.classList.remove("active");
   searchBtn.classList.remove("active");
   searchInput.classList.remove("active");
   cancelBtn.classList.remove("active");
-  searchData.classList.toggle("active");
   searchInput.value = "";
+};
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function moreFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
